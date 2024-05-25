c2b169925b55c21e05b227b6222e0c64754d2b28
Joanna <joufengchen15@gmail.com>
2024-05-25 23:30:58 +0800

    [feat] 搜尋結果列表: 設置NavigationBar

--- GitHubRepository/RepositoryListView/RepositoryListViewController.swift
+++ GitHubRepository/RepositoryListView/RepositoryListViewController.swift
@@ -11,6 +11,37 @@ class RepositoryListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+}
+
+// MARK: - private functions
+private extension RepositoryListViewController {
+    
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+        configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.shadowColor = .clear
+
+        // 設定大標題文字屬性
+        appearance.largeTitleTextAttributes = [
+            .foregroundColor: UIColor.black,
+            .font: UIFont.boldSystemFont(ofSize: 34)
+        ]
+        
+        // 配置導航欄的標準外觀
+        navigationController?.navigationBar.standardAppearance = appearance
+
+        // 啟用大標題
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // 標題文字
+        navigationItem.title = "Repository Search"
     }
     
 }
