92ee02d164532807a720ccdbefbaf7174d9cff8d
Joanna <joufengchen15@gmail.com>
2024-05-26 12:21:42 +0800

    [feat] viewModel抓取Repository資料

--- GitHubRepository/RepositoryListView/RepositoryListViewModel.swift
+++ GitHubRepository/RepositoryListView/RepositoryListViewModel.swift
@@ -8,4 +8,21 @@
 import Foundation
 class RepositoryListViewModel {
     
+    var repositories = [Item]()
+
+    func searchRepositories(byName repositoryName: String) {
+        // 抓取Repository資料
+        networkManager.search(repositoryName: repositoryName) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let repositoryResponse):
+                self.repositories = repositoryResponse.items
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private let networkManager = NetworkManager()
+
 }
