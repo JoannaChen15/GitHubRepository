d6843aa61073001d528f56e2a08c64085c3ab9c4
Joanna <joufengchen15@gmail.com>
2024-05-26 22:22:06 +0800

    [feat] 建立 RepositoryDetailViewModel

---
+++ GitHubRepository/RepositoryDetailView/RepositoryDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryDetailViewModel.swift
+//  GitHubRepository
+//
+//  Created by joanna on 2024/5/26.
+//
+
+import Foundation
+
+struct RepositoryDetailViewModel {
+    var repositoryModel: Item
+}
